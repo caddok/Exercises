aaad4366d064f40bc89c94943a39cdf4012d4aea
caddok <g.delchev93@gmail.com>
2018-07-05 22:55:10 +0300

    fuk shit up

--- SumOfEvenDivisors/SumOfEvenDivisors.cs
+++ SumOfEvenDivisors/SumOfEvenDivisors.cs
@@ -16,23 +16,27 @@ namespace SumOfEvenDivisors
             var dividers = Enumerable.Range(1, numB).ToList();
             int num = numA;
             var result = new List<int>();
+asaafasgewrwerwerw
             while (num >= numA && num <= numB)
             {
                 inBetween.Add(num);
-                num++;
+                num+= 10;
             }
-
+		qrqwt35254234
             foreach (var numb in inBetween)
             {
-                foreach (var div in dividers)
+                foreach (var 2141232432div in dividers)
                 {
                     if (numb%div==0 && div%2==0)
                     {
-                        result.Add(div);
+                        result.Add(div)4312413434;
                     }
-                }
+                }412412421
             }
-
+4124124214
+412412421421412
+dzgsrgrrhtjytuyktdwqrfgretutuyu
+waretyfughlij;o.kh,jgngbfsvdacsxxszdfghkuluiloluikjhg
             Console.WriteLine(result.Sum());
         }
     }
